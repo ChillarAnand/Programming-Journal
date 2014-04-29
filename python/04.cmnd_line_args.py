3a91694490d53346653780b6de9d8ee7ddb5a48d
chillaranand <anand21nanda@gmail.com>
2014-04-29 20:56:24 +0530

    using command line arguments in python

---
+++ python/04.cmnd_line_args.py
@@ -0,0 +1,13 @@
+#! usr/bin/python
+
+import sys
+
+def main():
+    print 'Number of arguments: ', len(sys.argv)
+    print 'Arguments are:', str(sys.argv)
+
+
+
+if __name__ == "__main__":
+    main()
+
