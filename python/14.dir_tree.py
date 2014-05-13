31516c12c67a43259cde8015ee80a57f48c434f8
chillaranand <anand21nanda@gmail.com>
2014-05-13 17:45:15 +0530

    nn

--- python/14.dir_tree.py
+++ python/14.dir_tree.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 
+import  subprocess
 import os
 
 rootDir = 'test/'
@@ -7,19 +8,26 @@ rootDir = 'test/'
 for dirName, subdirList, fileList in os.walk(rootDir):
 #    print dirName, subdirList, fileList
 
-    os.system('pwd')
-    print dirName
+#    os.system('pwd')
+#    print dirName
 
     for fname in fileList:
 
         if '.pdf' in fname:
             #print('\t %s' % fname )
 
-            dir_name = os.path.splitext(fname)[0]
+            print fname
+            abs_path = os.path.abspath((fname)) 
+            print abs_path
+            base_path = os.path.basename((fname))
+            print base_path
+            dir_name = abs_path.replace(base_path, '',)
             print dir_name
-            os.system('pwd')
-            command = "cp " + fname + " " + dir_name
-            print command
+            #subprocess.call('pwd')
+           # command = "cp " + fname + " " + dir_name
+            #print command
+            #subprocess.call(command)
             #os.system(command)
+#            os.path.dirname(os.path.realpath(__file__))
             
-    os.system('cd ..')
+  #  os.system('cd ..')
