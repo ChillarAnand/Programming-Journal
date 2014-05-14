d2d2ee8ab5feeb1668406b833e257bb9304e7764
chillaranand <anand21nanda@gmail.com>
2014-05-14 22:27:28 +0530

    python regex

--- python/16.get_links.py
+++ python/16.get_links.py
@@ -1,6 +1,10 @@
 import lxml.html, urllib2, urlparse
+import sys
 
-url = "http://ipindia.nic.in/ipr/patent/journal_archieve/journal_2013/patent_journal_2013.htm"
+
+url = str(sys.argv[1])
+print url
+#url = "http://ipindia.nic.in/ipr/patent/journal_archieve/journal_2013/patent_journal_2013.htm"
 
 res = urllib2.urlopen(url)
 
