e5c9cfcfd72657bcd36f78d58da674ae6a6874de
chillaranand <anand21nanda@gmail.com>
2014-05-30 00:35:27 +0530

    Ruby on Rails Installtion on Ububntu

--- python/04.cmnd_line_args.py
+++ python/04.cmnd_line_args.py
@@ -5,6 +5,9 @@ import sys
 def main():
     print 'Number of arguments: ', len(sys.argv)
     print 'Arguments are:', str(sys.argv)
+    print sys.argv[0]
+    print sys.argv[1]
+    print sys.argv[2]
 
 
 
