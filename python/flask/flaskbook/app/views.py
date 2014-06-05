a26cd8d6859920322fb5811444156e839e22f00c
chillaranand <anand21nanda@gmail.com>
2014-06-05 18:09:06 +0530

    Added flask files

---
+++ python/flask/flaskbook/app/views.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    user = { 'nickname' : 'Anand' }
+    return render_template(
+            "index.html",
+            title = "home",
+            user = user)
---
+++ python/flask/flaskbook/run.py
@@ -0,0 +1,4 @@
+#! /home/anand/git/learning/python/flask/flask/bin/python
+
+from app import app
+app.run(debug = True)
