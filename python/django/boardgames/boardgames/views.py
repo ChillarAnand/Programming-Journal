b267f7bf8b54759687e06ad528244cb26e6d0886
chillaranand <anand21nanda@gmail.com>
2014-06-05 18:06:59 +0530

    Added django files

---
+++ python/django/boardgames/boardgames/views.py
@@ -0,0 +1,11 @@
+from django.views.generic.base import View
+#from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from datetime import date
+
+
+class HelloWorldView(View):
+    def get(self, request):
+        #return HttpResponse("Hello World!")
+        return render_to_response('helloworld.html', 
+                                    { 'date' : date.today() })
